468bf59b0393af3e51f4a885d782474d787e6f51
xtp <xtpeqii@hotmail.com>
2016-06-26 08:40:14 +0800

    add Plone.mm

---
+++ Plone.mm
@@ -0,0 +1,75 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1466854722537" ID="ID_1471797145" MODIFIED="1466856555106" TEXT="Plone">
+<node CREATED="1466856381090" ID="ID_953328326" MODIFIED="1466856409813" POSITION="left" TEXT="Diazo Theme">
+<node CREATED="1466855021603" ID="ID_1063968955" MODIFIED="1466855193260" TEXT="manifest.cfg">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      contains metadata about the theme
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1466855143194" ID="ID_1624197151" MODIFIED="1466855184830" TEXT="rules.xml">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      contains the theme rules
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1466855149982" ID="ID_1794274588" MODIFIED="1466855176721" TEXT="index.html">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      the static HTML of the theme.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1466856758921" ID="ID_1530356382" MODIFIED="1466859170370" POSITION="right" TEXT="content(resemble a db table)">
+<node CREATED="1466856558481" ID="ID_1920314273" MODIFIED="1466856583301" TEXT="content types">
+<node CREATED="1466856568787" ID="ID_420785142" MODIFIED="1466856591324" TEXT="News"/>
+<node CREATED="1466856592578" ID="ID_1321134793" MODIFIED="1466856596454" TEXT="Event"/>
+<node CREATED="1466856599209" ID="ID_914306846" MODIFIED="1466856602702" TEXT="..."/>
+<node CREATED="1466856605270" ID="ID_1319703401" MODIFIED="1466856612978" TEXT="custom content type">
+<node CREATED="1466856615013" ID="ID_1492212878" MODIFIED="1466856626975" TEXT="Schema"/>
+<node CREATED="1466856631222" ID="ID_1724440836" MODIFIED="1466856632150" TEXT="FTI"/>
+<node CREATED="1466856636599" ID="ID_1593552892" MODIFIED="1466856637557" TEXT="Views"/>
+</node>
+</node>
+<node CREATED="1466859187690" ID="ID_682861700" MODIFIED="1466859226009" TEXT="content framework">
+<node CREATED="1466859234545" ID="ID_1902158303" MODIFIED="1466859235465" TEXT="Dexterity "/>
+<node CREATED="1466859237545" ID="ID_1277165068" MODIFIED="1466859560086" TEXT="Archetypes(old)"/>
+</node>
+<node CREATED="1466856768791" ID="ID_934388338" MODIFIED="1466859157709" TEXT="content constitution">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A content type is a variety of object that can store information and is editable by users. We have different contenttypes to reflect the different kinds of information about which we need to collect and display information. Pages, folders, events, news items, files (binary) and images are all contenttypes.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1466859052040" ID="ID_210834870" MODIFIED="1466859056980" TEXT="Schema"/>
+<node CREATED="1466859060943" ID="ID_1700123264" MODIFIED="1466860614806" TEXT="FTI(Factory Type Information)"/>
+<node CREATED="1466859066140" ID="ID_331804773" MODIFIED="1466859067098" TEXT="Views"/>
+</node>
+</node>
+</node>
+</map>
