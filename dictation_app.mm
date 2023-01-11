f7bdba035e2ee4fc0c7e4e352ad163f3e7c83c53
Billy Xia <billy.xia@upfeat.com>
2023-01-10 22:52:31 -0700

    change

---
+++ dictation_app.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1592709019712" ID="ID_106831282" MODIFIED="1592709948445" TEXT="basic functions">
+<node CREATED="1592709882906" ID="ID_800358260" MODIFIED="1592709925902" POSITION="right" TEXT="check result/show diffs"/>
+<node CREATED="1592709973106" ID="ID_1641228540" MODIFIED="1592709985823" POSITION="right" TEXT="..."/>
+</node>
+</map>
